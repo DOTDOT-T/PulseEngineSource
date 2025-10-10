9f36209c7b6a77c0787e7aaf4d034d4c502fd73a
Dorian Lexteriaque <42521229+DOT66400@users.noreply.github.com>
2025-10-10 12:08:02 +0200

    static mesh now tell the shader they have no skeleton

--- src/PulseEngine/core/Meshes/StaticMesh.cpp
+++ src/PulseEngine/core/Meshes/StaticMesh.cpp
@@ -7,6 +7,8 @@ void StaticMesh::Update()
 
 void StaticMesh::Render(Shader *shader) const
 {
+    shader->SetBool("hasSkeleton", false);
+    EDITOR_LOG("render a static mesh")
     for(Mesh* msh : meshes)
     {
         msh->Draw(shader);
